b31c60fd5ecd8280f6fa8204437c6d7d8bc0bc98
MOSMekawy <mostafa.rmekawy@gmail.com>
2025-01-23 16:34:57 +0200

    setup next proxy to get around cors issue

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://ergast.com/api/:path*' // Proxy to Backend
+      }
+    ]
+  }
 };
 
 export default nextConfig;
